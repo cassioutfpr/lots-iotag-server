7c274b293171cf894ec6747b2e343ca84617f0cd
Cassio Morales <cassioutfpr@gmail.com>
2021-05-19 00:15:09 -0300

    Adicionei boto.

--- lots-iotag-server.py
+++ lots-iotag-server.py
@@ -2,6 +2,12 @@ import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import logging
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+import boto
+import calendar;
+import time;
+import os
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -13,14 +19,19 @@ def writeToDocument():
     for item in data_array['apnt']:
         arrayString += "\n" + "Veiculo:" + item['vehicle'] + ",Tipo:" + item['type_ap'] + ",Lat:" + str(item['lat']) + ",Lng:" + str(item['lng']) + ",Time:" + str(item['timestamp'])
 
-    
+
+    ts = calendar.timegm(time.gmtime())
+    print(ts)
+    conn = S3Connection(os.environ['AWS_ID'], os.environ['AWS_PASSWORD'])
+    b = conn.get_bucket('apontamentos')
+    k = Key(b)
+    k.key = str(ts) + '.txt'
+    k.set_contents_from_string(arrayString)
+
     f = open("apontamentos.txt", "a")
     f.write(arrayString)
     f.close()
 
-    f = open("apontamentosPy.py", "a")
-    f.write(arrayString)
-    f.close()
     return jsonify({"write": "success"})
 
 @app.route('/',  methods=['GET', 'POST', 'OPTIONS'])
@@ -28,20 +39,12 @@ def check():
     f = open("apontamentos.txt", "r")
     return jsonify({"answer": f.read()})
 
-@app.route('/py',  methods=['GET', 'POST', 'OPTIONS'])
-def checkPy():
-    f = open("apontamentosPy.py", "r")
-    return jsonify({"answer": f.read()})
 
 @app.route('/erase-document-4679',  methods=['POST', 'OPTIONS'])
 def erase():
     f = open('apontamentos.txt', 'w').close()
     return jsonify({"answer": 'apagado'})
 
-@app.route('/erasePy',  methods=['GET', 'POST', 'OPTIONS'])
-def erasePy():
-    f = open('apontamentosPy.py', 'w').close()
-    return jsonify({"answer": 'apagado'})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
